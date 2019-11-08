9cad3ef3accf52f1efd23313b8e99b98decaff3c
wangzhengbo2 <wangzhengbo2@jd.com>
2019-11-08 18:25:31 +0800

    Add flow-typed support.

---
+++ js/flow/src/lodash.js
@@ -0,0 +1,11 @@
+// @flow
+
+import _ from 'lodash'
+import u from 'underscore'
+import $ from 'jquery'
+
+const maxValue: number = _.max([1, 2, 3])
+const maxV: number = u.max([1, 2, 3])
+
+$('body').click(function() {
+})
